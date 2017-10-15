e76a48b85c6ef20639ccda469a834528341ceeee
Qianchen He <qianchen@Qianchens-MacBook-Pro.local>
2017-10-14 20:50:26 -0700

    Assignment 5 extra problem

---
+++ Assignment5/src/Extra.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Extra {
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int c = matrix[0].length;
+        int r = matrix.length;
+        if (r == 0) {
+            return res;
+        }
+        int column = -1, row = 0;
+
+        while (true) {
+            for (int i = 0; i < c; i++) {
+                res.add(matrix[row][++column]);
+            }
+            if (--r == 0) break;
+            for (int i = 0; i < r; i++) {
+                res.add(matrix[++row][column]);
+            }
+            if (--c == 0) break;
+            for (int i = 0; i < c; i++) {
+                res.add(matrix[row][--column]);
+            }
+            if (--r == 0) break;
+            for (int i = 0; i < r; i++) {
+                res.add(matrix[--row][column]);
+            }
+            if (--c == 0) break;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] m = {{1,2,3},
+        {4,5,6},
+        {7,8,9}};
+        List<Integer> list = spiralOrder(m);
+        for (Integer l : list) {
+            System.out.print(l);
+        }
+        
+    }
+}
