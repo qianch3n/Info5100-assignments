7390b84c3f87d9e93300159fc5394acb37608fc4
Qianchen <hieverest@foxmail.com>
2017-10-20 00:12:28 -0700

    Assignment 6

--- Assignment6/Question3/src/main/java/com/q3/app/App.java
+++ Assignment6/Question3/src/main/java/com/q3/app/App.java
@@ -1,5 +1,12 @@
 package com.q3.app;
 
+//Hi Harsha,
+//
+//Please note: I use Maven under intelliJ to build this project as I need to use the
+//jackson dependency to write json file. So please test run the App in intelliJ. All
+//the user data would be stored in file called user_data.json. Sorry for the inconvenience.
+
+
 
 public class App 
 {
